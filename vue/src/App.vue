2643f0ac282aa7face4f327c5f739926f03f4a6e
Bryan McLain <bryan.r.mclain@gmail.com>
2021-09-21 15:02:03 -0400

    merge conflict 10002

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
+      <div class="logo">
+        <img src="@/assets/logo.png" alt="Abacus Logo">
+      </div>
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
     </div>
