af2df403c064858f326cb2321eeb6773db76947f
Shay Parker <parker.shayj@gmail.com>
2021-09-22 11:38:32 -0400

    LoginIconsRedo

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -3,13 +3,20 @@
     <div id="nav">
       <div class="logo">
         <img src="@/assets/logo.png" alt="Abacus Logo">
+      </div>
+      <div>
         <h4>Always gaining knowledge</h4>
-        </div>
-      
+      </div>
       <div class="nav-text">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>
-      <router-link v-bind:to="{ name: 'About' }">About</router-link>
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+      <router-link v-bind:to="{ name: 'home' }">
+        <img src="@/assets/homeIcon.png" alt="Home">
+      </router-link>
+      <router-link v-bind:to="{ name: 'About' }">
+        <img src="@/assets/infoIcon.png" alt="About">
+      </router-link>
+      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">
+        <img src="@/assets/logOutIcon.png" alt="Logout">
+      </router-link>
       </div>
     </div>
     <router-view />
